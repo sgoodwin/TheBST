abfdf30402e3a4c8a52e882c2feb01cb4a62fc8b
Samuel Goodwin <samuel@roundwallsoftware.com>
2022-01-03 15:56:17 +0100

    Don't let users edit/update/destroy other people's listings
    Might need to add in an exception for admins/mods later, but this will
    prevent people for now.

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -17,6 +17,9 @@ class ListingsController < ApplicationController
 
   # GET /listings/1/edit
   def edit
+    unless @listing.user == current_user
+      redirect_to root_url
+    end
   end
 
   # POST /listings or /listings.json
@@ -37,35 +40,46 @@ class ListingsController < ApplicationController
 
   # PATCH/PUT /listings/1 or /listings/1.json
   def update
-    respond_to do |format|
-      if @listing.update(listing_params)
-        format.html { redirect_to listing_url(@listing), notice: "Listing was successfully updated." }
-        format.json { render :show, status: :ok, location: @listing }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @listing.errors, status: :unprocessable_entity }
+
+    unless @listing.user == current_user
+      redirect_to root_url
+    else
+
+      respond_to do |format|
+        if @listing.update(listing_params)
+          format.html { redirect_to listing_url(@listing), notice: "Listing was successfully updated." }
+          format.json { render :show, status: :ok, location: @listing }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @listing.errors, status: :unprocessable_entity }
+        end
       end
     end
   end
 
   # DELETE /listings/1 or /listings/1.json
   def destroy
-    @listing.destroy
+    unless @listing.user == current_user
+      redirect_to root_url
+    else
 
-    respond_to do |format|
-      format.html { redirect_to listings_url, notice: "Listing was successfully destroyed." }
-      format.json { head :no_content }
+      @listing.destroy
+
+      respond_to do |format|
+        format.html { redirect_to listings_url, notice: "Listing was successfully destroyed." }
+        format.json { head :no_content }
+      end
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_listing
-      @listing = Listing.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def listing_params
-      params.require(:listing).permit(:title, :info, :price, :currency)
-    end
+  # Only allow a list of trusted parameters through.
+  def listing_params
+    params.require(:listing).permit(:title, :info, :price, :currency)
+  end
 end
