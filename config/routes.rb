458a41bb975943ffa6f0476849c88d22f0f00531
Joël Dimbernat <joel@dimbernat.net>
2022-01-08 18:39:47 +0100

    allows the user to mark a listing active again

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get "search", to: "listings#search", as: "search"
   post "mark_as_sold/:id", to: "listings#sold", as: "mark_as_sold"
   post "mark_as_cancelled/:id", to: "listings#cancel", as: "mark_as_cancel"
+  post "mark_as_active/:id", to: "listings#active", as: "mark_as_active"
 
   post "login", to: "users#login"
 end
